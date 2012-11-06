9be1c1b877ba0bbc66a777a5e88be4ddfb1b9eef
Márk Verebélyi <mark.verebelyi@gmail.com>
2012-11-06 20:55:45 +0100

    Namespace handling continued

--- src/main/java/org/springframework/integration/disruptor/config/DisruptorChannelFactoryBean.java
+++ src/main/java/org/springframework/integration/disruptor/config/DisruptorChannelFactoryBean.java
@@ -1,5 +1,13 @@
 package org.springframework.integration.disruptor.config;
 
+import java.util.List;
+
+import org.springframework.integration.channel.ChannelInterceptor;
+
 public class DisruptorChannelFactoryBean {
+
+	public void setInterceptors(final List<ChannelInterceptor> interceptors) {
+
+	}
 
 }
---
+++ src/test/java/org/springframework/integration/disruptor/config/DisruptorNamespaceIntegrationTest.java
@@ -0,0 +1,16 @@
+package org.springframework.integration.disruptor.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "/spring/namespace-test-config.xml" })
+public class DisruptorNamespaceIntegrationTest {
+
+	@Test
+	public void Config_OK() {
+	}
+
+}
