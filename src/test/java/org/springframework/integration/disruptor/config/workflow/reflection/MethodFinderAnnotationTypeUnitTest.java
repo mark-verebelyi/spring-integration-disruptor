8c9f58390f91d311a3a4ea49fb8c4d0ca6f0c94c
Márk Verebélyi <mark.verebelyi@gmail.com>
2012-11-19 16:52:29 +0100

    AnnotationType matching in MethodFinder

---
+++ src/test/java/org/springframework/integration/disruptor/config/workflow/reflection/MethodFinderAnnotationTypeUnitTest.java
@@ -0,0 +1,76 @@
+package org.springframework.integration.disruptor.config.workflow.reflection;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.util.ReflectionUtils;
+
+public class MethodFinderAnnotationTypeUnitTest {
+
+	private final SingleAnnotatedMethod singleAnnotatedMethod = new SingleAnnotatedMethod();
+	private final MultipleAnnotatedMethods multipleAnnotatedMethods = new MultipleAnnotatedMethods();
+
+	private final MethodFinder methodFinder = new MethodFinderImpl();
+
+	@Test
+	public void Single_annotation_type() {
+
+		final MethodSpecification specification = new MethodSpecification();
+		specification.setAnnotationType(SomeAnnotation.class);
+
+		final List<Method> methods = this.methodFinder.findMethods(this.singleAnnotatedMethod, specification);
+		assertEquals(1, methods.size());
+
+		assertEquals(ReflectionUtils.findMethod(SingleAnnotatedMethod.class, "doWhatEver"), methods.get(0));
+
+	}
+
+	@Test
+	public void Multiple_annotated_methods() {
+
+		final MethodSpecification specification = new MethodSpecification();
+		specification.setAnnotationType(SomeAnnotation.class);
+
+		final List<Method> methods = this.methodFinder.findMethods(this.singleAnnotatedMethod, specification);
+		assertEquals(1, methods.size());
+
+	}
+
+	public static class SingleAnnotatedMethod {
+
+		@SomeAnnotation
+		public void doWhatEver() {
+		}
+
+		public void doAnother() {
+		}
+
+	}
+
+	public static class MultipleAnnotatedMethods extends SingleAnnotatedMethod {
+
+		public int newInt() {
+			return 0;
+		}
+
+		@SomeAnnotation
+		public Object newObject() {
+			return new Object();
+		}
+
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target(ElementType.METHOD)
+	public static @interface SomeAnnotation {
+
+	}
+
+}
