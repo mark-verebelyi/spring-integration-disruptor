d7c00fdedddd3b8d0bca5f2363ae458a0273489d
Márk Verebélyi <mark.verebelyi@gmail.com>
2012-11-30 21:33:28 +0100

    Removed unnecessary component from ForwardingEventHandler

--- src/main/java/org/springframework/integration/disruptor/ForwardingEventHandler.java
+++ src/main/java/org/springframework/integration/disruptor/ForwardingEventHandler.java
@@ -4,9 +4,8 @@ import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
 import org.springframework.integration.core.MessagingTemplate;
 import org.springframework.integration.endpoint.AbstractEndpoint;
-import org.springframework.integration.mapping.InboundMessageMapper;
-import org.springframework.integration.support.MessageBuilder;
 import org.springframework.integration.support.converter.MessageConverter;
+import org.springframework.integration.support.converter.SimpleMessageConverter;
 import org.springframework.integration.transformer.Transformer;
 import org.springframework.util.Assert;
 
@@ -58,14 +57,9 @@ public final class ForwardingEventHandler extends AbstractEndpoint implements Ev
 		this.messagingTemplate = messagingTemplate;
 	}
 
-	/**
-	 * Fall-back mechanism when no {@link MessageConverter} has been configured.
-	 */
-	private final InboundMessageMapper<Object> inboundMessageMapper = new InboundMessageMapperImpl();
-
 	public void onEvent(final Object event, final long sequence, final boolean endOfBatch) throws Exception {
-		final Message<?> message = this.convert(event);
-		this.messagingTemplate.send(this.transform(message));
+		final Message<?> message = this.transform(this.convert(event));
+		this.messagingTemplate.send(message);
 	}
 
 	private Message<?> transform(final Message<?> message) {
@@ -83,7 +77,7 @@ public final class ForwardingEventHandler extends AbstractEndpoint implements Ev
 		} else if (event instanceof MessagingEvent) {
 			return ((MessagingEvent) event).getPayload();
 		} else {
-			return (this.converter != null ? this.converter.toMessage(event) : this.inboundMessageMapper.toMessage(event));
+			return this.converter.toMessage(event);
 		}
 	}
 
@@ -91,17 +85,9 @@ public final class ForwardingEventHandler extends AbstractEndpoint implements Ev
 	protected void onInit() throws Exception {
 		Assert.isTrue(this.channel != null, "'channel' attribtue is mandatory.");
 		this.messagingTemplate.setDefaultChannel(this.channel);
-		if (this.converter != null) {
-			this.messagingTemplate.setMessageConverter(this.converter);
+		if (this.converter == null) {
+			this.converter = new SimpleMessageConverter();
 		}
-	}
-
-	private static final class InboundMessageMapperImpl implements InboundMessageMapper<Object> {
-
-		public Message<?> toMessage(final Object object) throws Exception {
-			return MessageBuilder.withPayload(object).build();
-		}
-
 	}
 
 	@Override
